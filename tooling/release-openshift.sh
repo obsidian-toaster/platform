80e79adf63223994e4131adf96b615fdce45d2d5
Charles Moulliard <cmoulliard@apache.org>
2016-12-22 23:06:37 +0100

    Add instructions to deploy on OpenShift.

--- tooling/release-openshift.sh
+++ tooling/release-openshift.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-: ${1:?"Must specify release version. Ex: 1.0.0.alpha1"}
+: ${1:?"Must specify release version. Ex: 1.0.0.Alpha1"}
 api=${2:-https://api.engint.openshift.com}
 token=${3:-hRXsHSHzxwjiOUFbuvkgjIg2NWNHINuEeblNxq4zZ84}
 
@@ -12,18 +12,20 @@ sed -e "s/VERSION/$REL/g" ./templates/backend.yml > ./templates/backend-$REL.yml
 sed -e "s/VERSION/$REL/g" ./templates/front.yml > ./templates/front-$REL.yml
 
 # Log on to OpenShift
-# oc login $api --token=$token
-#
-# oc new-project obsidian-alpha1
-#
-# # Deploy the backend
-# oc create -f ./templates/backend-$REL.yml
-# oc process backend-generator-s2i | oc create -f -
-# oc start-build generator-backend-s2i
-#
-# # Deploy the Front
-# oc create -f templates/front-$REL.yml
-# oc process front-generator-s2i | oc create -f -
-# oc start-build front-generator
+oc login $api --token=$token
+oc new-project obsidian-alpha1
+sleep 5
+
+# Deploy the backend
+echo "Deploy the backend"
+oc create -f ./templates/backend-$REL.yml
+oc process backend-generator-s2i | oc create -f -
+oc start-build generator-backend-s2i
+
+# Deploy the Front
+echo "Deploy the frontend"
+oc create -f templates/front-$REL.yml
+oc process front-generator-s2i | oc create -f -
+oc start-build front-generator-s2i
 
 
