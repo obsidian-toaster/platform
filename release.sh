1d6381710c04cd0083f5c5a7434dfa9ecb323e13
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-12-22 16:33:48 +0100

    now generates front-end and pushes to github pages

--- release.sh
+++ release.sh
@@ -2,9 +2,11 @@
 
 : ${1:?"Must specify release version. Ex: 2.0.1.Final"}
 : ${2:?"Must specify next development version. Ex: 2.0.2-SNAPSHOT"}
+: ${3:?"Must specify backend url. Ex: http://generator-backend.myhost.io"}
 
 REL=$1
 DEV=$2
+FORGE_URL=$3
 
 WORK_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 echo "Working in temp directory $WORK_DIR"
@@ -43,8 +45,22 @@ git clone https://github.com/obsidian-toaster/generator-frontend.git
 cd generator-frontend
 npm install package-json-io
 node -e "var pkg = require('package-json-io'); pkg.read(function(err, data) { data.version = '$REL'; pkg.update(data, function(){}); })"
-git commit -m "released $REL of generator-frontend"
-git tag "$REL"
+git commit -a -m "released $REL of generator-frontend"
+git clone https://github.com/obsidian-toaster/obsidian-toaster.github.io.git build
+cd build
+git branch "release-$REL"
+git checkout "release-$REL"
+rm -rf *
+cd -
+npm install
+npm run build:prod
+cp -r dist/* build
+cd build
+git commit -a -m "released $REL of generator-frontend"
+git push origin "release-$REL"
+cd -
+
+git tag "release-$REL"
 git push origin --tags
 npm publish .
 
