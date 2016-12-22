c740c397ce67b866b27d79b8f406a5d41eeb845b
Charles Moulliard <cmoulliard@apache.org>
2016-12-22 16:59:40 +0100

    Save change to a new file which has the suffic = version

--- tooling/release-openshift.sh
+++ tooling/release-openshift.sh
@@ -8,22 +8,22 @@ REL=$1
 echo "Version : $REL"
 
 # Change version
-sed -i -e "s/VERSION/$REL/g" ./templates/backend.yml > ./templates/backend-$REL.yml
-sed -i -e "s/VERSION/$REL/g" ./templates/front.yml > ./templates/front-$REL.yml
+sed -e "s/VERSION/$REL/g" ./templates/backend.yml > ./templates/backend-$REL.yml
+sed -e "s/VERSION/$REL/g" ./templates/front.yml > ./templates/front-$REL.yml
 
 # Log on to OpenShift
-oc login $api --token=$token
-
-oc new-project obsidian-alpha1
-
-# Deploy the backend
-oc create -f ./templates/backend-$REL.yml
-oc process backend-generator-s2i | oc create -f -
-oc start-build generator-backend-s2i
-
-# Deploy the Front
-oc create -f templates/front-$REL.yml
-oc process front-generator-s2i | oc create -f -
-oc start-build front-generator
+# oc login $api --token=$token
+#
+# oc new-project obsidian-alpha1
+#
+# # Deploy the backend
+# oc create -f ./templates/backend-$REL.yml
+# oc process backend-generator-s2i | oc create -f -
+# oc start-build generator-backend-s2i
+#
+# # Deploy the Front
+# oc create -f templates/front-$REL.yml
+# oc process front-generator-s2i | oc create -f -
+# oc start-build front-generator
 
 
