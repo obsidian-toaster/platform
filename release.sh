34ee5f3e427f237ff0b21035199a73693f541566
Charles Moulliard <cmoulliard@apache.org>
2017-01-10 15:39:39 +0100

    Add TODO

--- release.sh
+++ release.sh
@@ -24,6 +24,7 @@ function mvnRelease {
 echo Press any key to release the Quickstarts...
 read junk
 # Release Quickstarts - TODO: no need to release:perform it
+// TODO - Release the archetypes on Maven Central instead of JBoss Repository Maven Nexus
 mvnRelease https://github.com/obsidian-toaster-quickstarts/quick_rest_vertx.git quick_rest_vertx
 mvnRelease https://github.com/obsidian-toaster-quickstarts/quick_rest_springboot-tomcat.git quick_rest_springboot-tomcat
 mvnRelease https://github.com/obsidian-toaster-quickstarts/quick_secured_rest-springboot.git quick_secured_rest-springboot
@@ -32,6 +33,7 @@ echo Press any key to release the Platform...
 read junk
 
 # Release Platform. Archetypes should be previously generated and pushed
+// TODO - Step missing - Should be investigated by George Gastaldi
 mvnRelease https://github.com/obsidian-toaster/platform platform
 
 echo Press any key to release the Obsidian addon...
@@ -44,33 +46,14 @@ echo Press any key to release the Backend...
 read junk
 
 # Release Backend
+// TODO - replace value using sed (George Gastaldi)
 mvnRelease https://github.com/obsidian-toaster/generator-backend.git generator-backend
 
 echo Press any key to release the Frontend...
 read junk
 
 # Release Frontend
-git clone https://github.com/obsidian-toaster/generator-frontend.git
-cd generator-frontend
-npm install package-json-io
-node -e "var pkg = require('package-json-io'); pkg.read(function(err, data) { data.version = '$REL'; pkg.update(data, function(){}); })"
-git commit -a -m "Released $REL of generator-frontend"
-git clone https://github.com/obsidian-toaster/obsidian-toaster.github.io.git build
-cd build
-git checkout -b "$REL"
-rm -rf *
-cd -
-npm install
-npm run build:prod
-cp -r dist/* build
-cd build
-git add .
-git commit -a -m "Released $REL of generator-frontend"
-git push origin "$REL"
-cd -
-git tag "$REL"
-git push origin --tags
-npm publish .
+// TODO - use the release_front.sh script
 
 # clean up
 echo "Cleaning up temp directory $WORK_DIR"
