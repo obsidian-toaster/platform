7a48f732333d3d29178a903c3aa605decb3c1847
Charles Moulliard <cmoulliard@apache.org>
2017-01-11 11:35:19 +0100

    Add line removed

--- tooling/openshift/provision-openshift.sh
+++ tooling/openshift/provision-openshift.sh
@@ -59,6 +59,7 @@ mkdir -p /etc/systemd/system/docker.service.d
 
 cat > /etc/systemd/system/docker.service.d/override.conf << __EOF__
 [Service]
+ExecStart=
 ExecStart=/usr/bin/docker daemon --storage-driver=overlay --insecure-registry 172.30.0.0/16
 __EOF__
 
