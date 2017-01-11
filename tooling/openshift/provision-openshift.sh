89a5b537a60d8326522fbee95adeeaa8d449a54e
Charles Moulliard <cmoulliard@apache.org>
2017-01-11 15:10:22 +0100

    Add test to include external nameserver

--- tooling/openshift/provision-openshift.sh
+++ tooling/openshift/provision-openshift.sh
@@ -147,3 +147,11 @@ __EOF__
 systemctl daemon-reload
 systemctl enable openshift-origin
 systemctl start openshift-origin
+
+if [ $host != "local" ]; then
+  echo "===================================================="
+  echo "Add external nameserver & restart docker"
+  echo "===================================================="
+  echo 'nameserver 8.8.8.8' >>  /etc/resolv.conf
+  service docker restart
+fi
