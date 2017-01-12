b370758ec9475b3367cbd6b540039d3eaa0a2419
Charles Moulliard <cmoulliard@apache.org>
2017-01-12 18:22:38 +0100

    Add missing /auth. Include minishift example

--- tooling/scripts/quickstart_sb_secured.sh
+++ tooling/scripts/quickstart_sb_secured.sh
@@ -4,6 +4,13 @@
 # Token                         --> ./quickstart_sb_secured.sh -a https://api.engint.openshift.com -t aehZPpWNbaMb3nfXaxAKNkp_o8qCLcwGRmdbGM9SqPU -c http://secured-springboot-rest-sso.e8ca.engint.openshiftapps.com -s https://secure-sso-sso.e8ca.engint.openshiftapps.com
 # User/password                 --> ./quickstart_sb_secured.sh -a 172.16.50.40:8443 -u admin -p admin -c http://secured-springboot-rest-sso.172.16.50.40.xip.io -s https://secure-sso-sso.172.16.50.40.xip.io
 # User/password (local vagrant) --> ./quickstart_sb_secured.sh -a 172.28.128.4:8443 -u admin -p admin -c http://secured-springboot-rest-sso.172.28.128.4.xip.io -s https://secure-sso-sso.172.28.128.4.xip.io
+# Minishift                     --> ./quickstart_sb_secured.sh -a 192.168.64.25:8443 -u admin -p admin -c http://secured-springboot-rest-sso.192.168.64.25:8443.xip.io -s https://secure-sso-sso.192.168.64.25:8443.xip.io
+#
+# ./httpie/token_req.sh https://secure-sso-sso.192.168.64.25.xip.io http://secured-springboot-rest-sso.192.168.64.25.xip.io
+
+
+
+
 
 while getopts a:t:u:p:c:s: option
 do
@@ -41,7 +48,7 @@ cd quick_secured_rest-springboot
 mvn clean install -Popenshift
 cd sso
 mvn fabric8:deploy -Popenshift
-oc env dc/secured-springboot-rest SSO_URL=$sso
+oc env dc/secured-springboot-rest SSO_URL=$sso/auth
 cd ../
 sleep 5
 echo "Endpoint : $app & SSO : $sso"
