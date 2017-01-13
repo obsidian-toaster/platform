3acff986de2cd45f4aa328f59ce9617896d351c2
Charles Moulliard <cmoulliard@apache.org>
2017-01-13 19:30:38 +0100

    Fix issue with FORGE_URL

--- tooling/release/release-openshift.sh
+++ tooling/release/release-openshift.sh
@@ -3,9 +3,9 @@
 # Example :
 # Token                         --> ./quickstart_sb.sh -a https://api.engint.openshift.com -t xxxxxxxxxxxx -v 1.0.0.Alpha1
 # User/password (CI Server)     --> ./quickstart_sb.sh -a https://172.16.50.40:8443 -u admin -p admin  -v 1.0.0.Alpha1
-# User/password (local vagrant) --> ./quickstart_sb.sh -a 172.28.128.4:8443 -u admin -p admin  -v 1.0.0.Alpha1
+# User/password (local vagrant) --> ./quickstart_sb.sh -a 172.28.128.4:8443 -u admin -p admin  -v 1.0.0.Alpha1 -b backend-generator-obsidian-alpha1.172.28.128.4.xip.io
 
-while getopts a:t:u:p:v: option
+while getopts a:t:u:p:v:b: option
 do
         case "${option}"
         in
@@ -14,6 +14,8 @@ do
                 u) user=${OPTARG};;
                 p) password=${OPTARG};;
                 v) version=${OPTARG};;
+                b) backendurl=${OPTARG};;
+
         esac
 done
 
@@ -29,12 +31,15 @@ fi
 
 REL=$version
 echo "Version : $REL"
+echo "Backend : $backendurl"
 
 # Change version
 sed -e "s/VERSION/$REL/g" ./templates/backend.yml > ./templates/backend-$REL.yml
-sed -e "s/VERSION/$REL/g" ./templates/front.yml > ./templates/front-$REL.yml
+sed -e "s/VERSION/$REL/g" -e "s/GENERATOR_URL/http:\/\/$backendurl/g" ./templates/front.yml > ./templates/front-$REL.yml
 
-# Remove first 6 chars (metadata.name: must match the regex [a-z0-9]([-a-z0-9]*[a-z0-9])? (e.g. 'my-name' or '123-abc')
+#
+# Remove first 6 chars otherwise OpenShift will complaints --> metadata.name: must match the regex [a-z0-9]([-a-z0-9]*[a-z0-9])? (e.g. 'my-name' or '123-abc')
+#
 suffix=${REL:6}
 suffix_lower=$(echo $suffix | tr '[:upper:]' '[:lower:]')
 
