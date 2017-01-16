556ae601098d3e6173c747e9ae79a70bccdcd4e5
Charles Moulliard <cmoulliard@apache.org>
2017-01-16 16:00:05 +0100

    Add script to delete and/or fork obsidian repos for dummy release tests

---
+++ tooling/release/fork_repo.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+: ${1:?"Must specify a github username."}
+: ${2:?"Must specify the password"}
+: ${3:?"Organisation where repo will be forked."}
+: ${4:?"Delete forked repo first"}
+
+USERNAME=$1
+PASSWORD=$2
+ORG=$3
+TO_BE_DELETED=$4
+
+githubRepos=("obsidian-toaster-quickstarts/quick_rest_vertx" \
+"obsidian-toaster-quickstarts/quick_rest_springboot-tomcat"  \
+"obsidian-toaster-quickstarts/quick_secured_rest-springboot"  \
+"obsidian-toaster/platform"  \
+"obsidian-toaster/obsidian-addon"  \
+"obsidian-toaster/generator-backend"  \
+"obsidian-toaster/generator-frontend")
+
+githubReposForked=("quick_rest_vertx" \
+"quick_rest_springboot-tomcat"  \
+"quick_secured_rest-springboot"  \
+"platform"  \
+"obsidian-addon"  \
+"generator-backend" \
+"generator-frontend")
+
+for repo in "${githubReposForked[@]}"
+do
+   if [ $TO_BE_DELETED = true ]; then
+      echo "Forked repo to be deleted : $repo"
+      curl -u $USERNAME:$PASSWORD -X DELETE https://api.github.com/repos/$ORG/$repo
+   fi
+done
+
+sleep 5
+echo Press any key to fork the repos
+read junk
+for repo in "${githubRepos[@]}"
+do
+   #echo "curl -u $USERNAME:$PASSWORD -X POST https://api.github.com/repos/$repo/forks?org=$ORG"
+   curl -u $USERNAME:$PASSWORD -X POST https://api.github.com/repos/$repo/forks?org=$ORG
+done
