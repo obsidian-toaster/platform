e35750498293308cd0ac032e3ca90e220b73a5d0
Charles Moulliard <cmoulliard@apache.org>
2017-01-30 17:09:12 +0100

    Simplify code to maintain only one collection of githubprojects

--- tooling/release/fork_repo.sh
+++ tooling/release/fork_repo.sh
@@ -11,7 +11,8 @@ read -s -p "Enter Password: " PASSWORD
 
 echo "$USERNAME : $PASSWORD"
 
-githubRepos=("obsidian-toaster-quickstarts/rest_vertx" \
+githubRepos=(
+"obsidian-toaster-quickstarts/rest_vertx" \
 "obsidian-toaster-quickstarts/rest_springboot-tomcat"  \
 "obsidian-toaster-quickstarts/secured_rest-springboot"  \
 "obsidian-toaster-quickstarts/rest_wildfly-swarm" \
@@ -19,22 +20,14 @@ githubRepos=("obsidian-toaster-quickstarts/rest_vertx" \
 "obsidian-toaster/obsidian-addon"  \
 "obsidian-toaster/generator-backend"  \
 "obsidian-toaster/generator-frontend" \
-"obsidian-toaster/obsidian-toaster.github.io")
+"obsidian-toaster/obsidian-toaster.github.io"
+)
 
-githubReposForked=("rest_vertx" \
-"rest_springboot-tomcat"  \
-"rest_wildfly-swarm" \
-"secured_rest-springboot"  \
-"platform"  \
-"obsidian-addon"  \
-"generator-backend" \
-"generator-frontend" \
-"obsidian-toaster.github.io")
-
-for repo in "${githubReposForked[@]}"
+for repo in "${githubRepos[@]}"
 do
    if [ $TO_BE_DELETED = true ]; then
-      echo "Forked repo to be deleted : $repo"
+      IFS='/' read -r -a array <<< "$repo"
+      echo "Forked repo to be deleted : ${array[1]}"
       curl -u $USERNAME:$PASSWORD -X DELETE https://api.github.com/repos/$ORG/$repo
    fi
 done
