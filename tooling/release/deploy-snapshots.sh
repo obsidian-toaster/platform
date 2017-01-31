65b759970d3ace3dc969ae3edee2b7f6edd31202
Charles Moulliard <cmoulliard@apache.org>
2017-01-31 19:07:21 +0100

    Add -DskipTests=true option. Deploy quickstarts snapshots. Remove option -DserverId= which is not used

--- tooling/release/deploy-snapshots.sh
+++ tooling/release/deploy-snapshots.sh
@@ -3,12 +3,15 @@
 #
 # Script responsible to build & publish the snapshots
 # Example :
-# ./deploy-snapshots.sh 1.0.0-SNAPSHOT nexus-infra.172.28.128.4.xip.io/content/repositories/snapshots openshift-nexus
+# ./deploy-snapshots.sh 1.0.0-SNAPSHOT nexus-infra.172.28.128.4.xip.io/content/repositories/snapshots
+#
+#
+# For local deployment, verify thatyou have added within the settings.xml file a server id for
+# <id>jboss-releases-repository</id> and <id>jboss-snapshots-repository</id> containing the username/password
 #
 
 : ${1:?"Must specify snapshot version. Ex: 1.0.0-SNAPSHOT"}
 : ${2:?"Must specify Alternate Maven Repo to publish. Ex: nexus-infra.172.28.128.4.xip.io/content/repositories/snapshots"}
-: ${3:?"Must specify the Maven Server Id. Ex: openshift-nexus"}
 
 DEV=$1
 ORG="obsidian-toaster-quickstarts"
@@ -24,14 +27,28 @@ cd $WORK_DIR
 function mvnDeploy {
   REPO=$1
   REPODIR=$2
+
   git clone $REPO $REPODIR
   cd $REPODIR
-  mvn clean deploy -DserverId=$MAVEN_REPO_ID -Djboss.snapshots.repo.url=http://$MAVEN_REPO -DskipStaging=true
+  mvn clean deploy -DskipTests=true -Djboss.snapshots.repo.url=http://$MAVEN_REPO -DskipStaging=true
   cd -
 }
 
+# Step 1. : Build, Deploy QuickStarts
 #
-# Step 1. : Release Platform. Archetypes should be previously generated and pushed
+echo Press any key to release the Quickstarts...
+read junk
+JSONFILE=$CURRENT/quickstarts.json
+START=0
+END=$(jq '. | length' $JSONFILE)
+for ((c=$START;c<=$END-1; c++ ))
+do
+  name=$(jq -r '.['$c'].name' $JSONFILE)
+  mvnDeploy https://github.com/$ORG/$name.git $name
+done
+
+#
+# Step 2. : Release Platform. Archetypes should be previously generated and pushed
 # Generate from the QuickStarts the Maven corresponding archetypes
 # Generate a Maven POM file containing the different archetypes to be used
 #
@@ -42,19 +59,19 @@ cd platform/archetype-builder
 mvn clean compile exec:java -Dgithub.organisation=$ORG
 
 cd ..
-mvn clean deploy -DserverId=$MAVEN_REPO_ID -Djboss.snapshots.repo.url=http://$MAVEN_REPO -DskipStaging=true
+mvn clean deploy -DskipTests=true -Djboss.snapshots.repo.url=http://$MAVEN_REPO -DskipStaging=true
 cd ..
 
 
 #
-# Step 2. : Release Obsidian Forge addon
+# Step 3. : Release Obsidian Forge addon
 #
 echo Press any key to deploy the Obsidian addon...
 read junk
 mvnDeploy https://github.com/obsidian-toaster/obsidian-addon.git obsidian-addon
 
 #
-# Step 3. : Release Backend (PROD is not required)
+# Step 4. : Release Backend (PROD is not required)
 #
 # CATALOG_URL : List of the Archetypes that we will use to generate the code (zip file downloaded by the user)
 #
@@ -63,7 +80,7 @@ read junk
 mvnDeploy https://github.com/obsidian-toaster/generator-backend.git generator-backend
 
 #
-# Step 4. : Release Frontend (PROD is not required)
+# Step 5. : Release Frontend (PROD is not required)
 # This is HTML/javascript project
 # It uses REST Api exposed by the backend to access the services
 # FORGE_URL : REST endpoint
