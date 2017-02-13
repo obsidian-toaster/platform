1e73e84361051b3c80764f854945d2c5fda91a01
Charles Moulliard <cmoulliard@apache.org>
2017-02-13 18:27:04 +0100

    Add example to release

--- tooling/release/release-openshift.sh
+++ tooling/release/release-openshift.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
 # Example :
-# Token                         --> ./release-openshift.sh -a api.engint.openshift.com -t xxxxxxxxxxxx -v 1.0.0.Alpha1 -g obsidian-toaster
-# User/password (CI Server)     --> ./release-openshift.sh -a 172.16.50.40:8443 -u admin -p admin  -v 1.0.0.Alpha1 -g obsidian-toaster
+# Token                         --> ./release-openshift.sh -a api.engint.openshift.com -t xxxxxxxxxxxx -v 1.0.0.Alpha1 -o obsidian-toaster
+# User/password (CI Server)     --> ./release-openshift.sh -a 172.16.50.40:8443 -u admin -p admin  -v 1.0.0.Alpha1 -o obsidian-toaster
 # User/password (local vagrant) --> ./release-openshift.sh -a 172.28.128.4:8443 -u admin -p admin -v 1.0.0.Dummy \
 #                                                          -b http://backend-generator-obsidian-dummy.172.28.128.4.xip.io/ \
 #                                                          -o obsidian-tester \
 #                                                          -c http://nexus-infra.172.28.128.4.xip.io/content/repositories/releases/org/obsidiantoaster/archetypes-catalog/1.0.0.Dummy/archetypes-catalog-1.0.0.Dummy-archetype-catalog.xml \
 #                                                          -n http://nexus-infra.172.28.128.4.xip.io
+#
+# Example to release Alpha2 of Obsidian on engint.openshift.com machine using the Jboss Nexus Server
+#
+# ./release-openshift.sh -a api.engint.openshift.com -t xxxxxxxxxxxxxxxxxxxxxx \
+#                        -v 1.0.0.Alpha2 \
+#                        -b http://backend-generator-obsidian-snapshot.e8ca.engint.openshiftapps.com/ \
+#                        -o obsidian-toaster \
+#                        -c 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases\&g=org.obsidiantoaster\&a=archetypes-catalog\&v=1.0.0.Alpha2\&e=xml\&c=archetype-catalog' \
+#                        -n http://repository.jboss.org/nexus
 
 while getopts a:t:u:p:v:b:o:c:n: option
 do
@@ -53,7 +62,7 @@ sed -e "s/VERSION/$REL/g" -e "s/ORG\//$githuborg\//g" -e "s|MAVENSERVER|$mavense
 sed -e "s/VERSION/$REL/g" -e "s|GENERATOR_URL|$backendurl|g" -e "s/ORG\//$githuborg\//g" ./templates/front.yml > ./templates/front-$REL.yml
 
 #
-# Remove first 6 chars otherwise OpenShift will complaints --> metadata.name: must match the regex [a-z0-9]([-a-z0-9]*[a-z0-9])? (e.g. 'my-name' or '123-abc')
+# Remove first 6 chars otherwise OpenShift will complain --> metadata.name: must match the regex [a-z0-9]([-a-z0-9]*[a-z0-9])? (e.g. 'my-name' or '123-abc')
 #
 suffix=${REL:6}
 suffix_lower=$(echo $suffix | tr '[:upper:]' '[:lower:]')
