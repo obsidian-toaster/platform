d7de01a196aa80fdc4ed281aee61faba30507c27
Charles Moulliard <ch007m@gmail.com>
2017-04-14 11:58:27 +0200

    Update custom_patch.sh
    Add identity = user/password

--- tooling/openshift/custom_patch.sh
+++ tooling/openshift/custom_patch.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash 
 
+#
+# Install Launchpad mission control within an openshift template using the parameters passed
+# Patch jenkins to use admin as role
+# bash <(curl -sL https://goo.gl/1XEYNb) hostIP projectName myGithubToken username:password 
+# example : bash <(curl -sL https://goo.gl/1XEYNb) 192.168.64.25 test1 cmoulliard mygithubtoken developer:developer
+#
 HOSTNAMEORIP=$1
 PROJECTNAME=${2:-myproject}
 GITHUB_USER=$3
 GITHUB_TOKEN=$4
 CONSOLE_URL=$(minishift console --url)
+IFS=':' read -a IDENTITY <<< "$5"
 
 echo "Host: $HOSTNAMEORIP"
 echo "Project: $PROJECTNAME"
 echo "Console: $CONSOLE_URL"
 echo "Github user: $GITHUB_USER"
+echo "Identity: ${IDENTITY[0]}, ${IDENTITY[1]}"
 
-# Log on to the platform using system:admin user
-oc login -u system:admin
+oc login -u ${IDENTITY[0]} -p ${IDENTITY[1]}
 oc project $PROJECTNAME
 
 # Install the launchpad-missioncontrol template
@@ -29,12 +36,14 @@ oc new-app launchpad -n $PROJECTNAME \
     -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_API_URL=$CONSOLE_URL \
     -p LAUNCHPAD_KEYCLOAK_URL= \
     -p LAUNCHPAD_KEYCLOAK_REALM= \
-    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_USERNAME=developer \
-    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_PASSWORD= \
+    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_USERNAME=$IDENTITY[0] \
+    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_PASSWORD=$IDENTITY[1] \
     -p LAUNCHPAD_BACKEND_CATALOG_GIT_REPOSITORY=https://github.com/openshiftio/booster-catalog.git \
     -p LAUNCHPAD_BACKEND_CATALOG_GIT_REF=master \
     -p LAUNCHPAD_BACKEND_CATALOG_INDEX_PERIOD="0" \
     -p LAUNCHPAD_FRONTEND_HOST=launchpad-frontend-$PROJECTNAME.$HOSTNAMEORIP.nip.io
 
 # Replace edit role with admin in order to allow the jenkins serviceaccount to by example create rolebindings, ...
+# Log on to the platform using system:admin user
+oc login -u system:admin
 oc patch template/jenkins-ephemeral -n openshift --type='json' -p='[{"op": "replace", "path": "/objects/3/roleRef/name", "value":"admin"}]'
