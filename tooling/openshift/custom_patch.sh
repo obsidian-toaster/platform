7f7955df78568499ddbe131f0ad7bb4764c1e025
Charles Moulliard <ch007m@gmail.com>
2017-04-14 11:45:30 +0200

    Update custom_patch.sh

--- tooling/openshift/custom_patch.sh
+++ tooling/openshift/custom_patch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/usr/bin/env bash 
 
 HOSTNAMEORIP=$1
 PROJECTNAME=${2:-myproject}
@@ -10,3 +10,31 @@ echo "Host: $HOSTNAMEORIP"
 echo "Project: $PROJECTNAME"
 echo "Console: $CONSOLE_URL"
 echo "Github user: $GITHUB_USER"
+
+# Log on to the platform using system:admin user
+oc login -u system:admin
+oc project $PROJECTNAME
+
+# Install the launchpad-missioncontrol template
+oc create -n $PROJECTNAME -f https://raw.githubusercontent.com/openshiftio/launchpad-templates/v2/openshift/launchpad-template.yaml
+
+# Local Deployment of  launchpad 
+# -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_API_URL=https://openshift.default.svc.cluster.local
+# -p LAUNCHPAD_KEYCLOAK_URL=https://sso.prod-preview.openshift.io/auth \
+# -p LAUNCHPAD_KEYCLOAK_REALM=fabric8 \
+oc new-app launchpad -n $PROJECTNAME \
+    -p LAUNCHPAD_MISSIONCONTROL_GITHUB_USERNAME=$GITHUB_USER \
+    -p LAUNCHPAD_MISSIONCONTROL_GITHUB_TOKEN=$GITHUB_TOKEN \
+    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_CONSOLE_URL=$CONSOLE_URL \
+    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_API_URL=$CONSOLE_URL \
+    -p LAUNCHPAD_KEYCLOAK_URL= \
+    -p LAUNCHPAD_KEYCLOAK_REALM= \
+    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_USERNAME=developer \
+    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_PASSWORD= \
+    -p LAUNCHPAD_BACKEND_CATALOG_GIT_REPOSITORY=https://github.com/openshiftio/booster-catalog.git \
+    -p LAUNCHPAD_BACKEND_CATALOG_GIT_REF=master \
+    -p LAUNCHPAD_BACKEND_CATALOG_INDEX_PERIOD="0" \
+    -p LAUNCHPAD_FRONTEND_HOST=launchpad-frontend-$PROJECTNAME.$HOSTNAMEORIP.nip.io
+
+# Replace edit role with admin in order to allow the jenkins serviceaccount to by example create rolebindings, ...
+oc patch template/jenkins-ephemeral -n openshift --type='json' -p='[{"op": "replace", "path": "/objects/3/roleRef/name", "value":"admin"}]'
