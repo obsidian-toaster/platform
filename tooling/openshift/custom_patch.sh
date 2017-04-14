41b1873c1178a46d148bbbc475c3c7c6f0412a70
Charles Moulliard <ch007m@gmail.com>
2017-04-14 11:11:36 +0200

    Update custom_patch.sh

--- tooling/openshift/custom_patch.sh
+++ tooling/openshift/custom_patch.sh
@@ -4,6 +4,7 @@ $HOSTNAMEORIP=$1
 $PROJECTNAME=myproject
 $GITHUB_USER=$2
 $GITHUB_TOKEN=$3
+$CONSOLE_URL=$(minishift console --url)
 
 # Log on to the platform using system:admin user
 oc login -u system:admin
@@ -19,16 +20,16 @@ oc patch template/jenkins-ephemeral -n openshift --type='json' -p='[{"op": "repl
 oc new-app launchpad -n openshift \
     -p LAUNCHPAD_MISSIONCONTROL_GITHUB_USERNAME=$GITHUB_USER \
     -p LAUNCHPAD_MISSIONCONTROL_GITHUB_TOKEN=$GITHUB_TOKEN \
-    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_CONSOLE_URL=https://$HOSTNAMEORIP:8443 \
+    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_CONSOLE_URL=$CONSOLE_URL \
+    -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_API_URL=$CONSOLE_URL \
+    # -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_API_URL=https://openshift.default.svc.cluster.local
     # -p LAUNCHPAD_KEYCLOAK_URL=https://sso.prod-preview.openshift.io/auth \
     # -p LAUNCHPAD_KEYCLOAK_REALM=fabric8 \
     -p LAUNCHPAD_KEYCLOAK_URL= \
     -p LAUNCHPAD_KEYCLOAK_REALM= \
-    # -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_API_URL=https://openshift.default.svc.cluster.local
     -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_USERNAME=developer \
     -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_PASSWORD= \
     -p LAUNCHPAD_BACKEND_CATALOG_GIT_REPOSITORY=https://github.com/openshiftio/booster-catalog.git \
     -p LAUNCHPAD_BACKEND_CATALOG_GIT_REF=master \
     -p LAUNCHPAD_BACKEND_CATALOG_INDEX_PERIOD="0" \
-    # -p LAUNCHPAD_BACKEND_URL=http://launchpad-backend-$PROJECTNAME.$HOSTNAMEORIP.nip.io
     -p LAUNCHPAD_BACKEND_URL=http://launchpad-backend-$PROJECTNAME.$HOSTNAMEORIP.nip.io
